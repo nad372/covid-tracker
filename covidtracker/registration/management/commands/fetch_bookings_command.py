6babc6e04c21e7fb8cb8993e8bce352b1cb50145
rishabhranawat <rishabhranawat12345@gmail.com>
2020-05-24 14:07:56 -0400

    sample save into db

--- covidtracker/registration/management/commands/fetch_bookings_command.py
+++ covidtracker/registration/management/commands/fetch_bookings_command.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from .fetch_bookings import BookingsFetcher
+from registration.models import Booking
 
 class Command(BaseCommand):
 
@@ -7,5 +8,16 @@ class Command(BaseCommand):
         bookingFetcher = BookingsFetcher()
         bookings = bookingFetcher.fetch()
 
-        print(len(bookings))
-        print(bookings[0])
+        for each in bookings:
+            _id = int(each['id'])
+            client = each['client']
+            client_email = each['client_email']
+            event = each['event']
+            unit = each['unit']
+            record_date = each[]
+
+            booking = Booking(id=_id, client=client, 
+                clientEmail=client_email,
+                event=event, unit=unit)
+            booking.save()
+            break
